a6f791a8272a48f83a4b9e7ac2d170c3c19a13a3
olalekan <olamilekanadekanmbi@gmail.com>
2022-10-15 14:37:07 +0100

    next and tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		"./pages/**/*.{html,js,jsx,tsx,ts}",
+		"./components/**/*.{html,js,jsx,tsx,ts}",
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
